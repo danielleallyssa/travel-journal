a60b11cb9c64e2625960336b74f470e2d9b0395a
Danielle Brown <hello@danielleallyssa.com>
2024-11-02 22:04:56 -0400

    Fixed image path

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,13 +1,11 @@
 import "./Map.css";
+import icon from "./assets/Pin.svg";
+
 const Map = (data) => {
   const { location, googleMapUrl } = data;
   return (
     <div className="feature--detail-content">
-      <img
-        className="feature--icon--image"
-        src="./src/assets/Pin.svg"
-        alt="icon image"
-      />
+      <img className="feature--icon--image" src={icon} alt="icon image" />
       <h3 className="feature--country">{location}</h3>
       <a className="feature--map" href={googleMapUrl}>
         View on Google Maps
